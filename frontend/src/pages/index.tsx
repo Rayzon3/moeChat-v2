7fa66bdaa47da310e7f4bc775cc195597d304815
Rahul Bhardwaj <rahul.03111999@gmail.com>
2023-01-07 15:09:52 +0530

    added google auth 🎉

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { signIn, useSession, signOut } from "next-auth/react"
+
 export default function Home() {
+
+  const { data } = useSession()
+
+  console.log("data: ", data)
+
   return (
-    <div>Hello World!</div>
+    <div>
+      {data?.user ? <button onClick={() => signOut()}>Sign Out</button> :  <button onClick={() => signIn('google')}>Sign In</button>}
+      {data?.user?.name}
+    </div>
   )
 }
