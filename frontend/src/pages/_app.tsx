cc5b89e1da30c7b6f46cb97b5d08a39fb2ca81a5
Rahul Bhardwaj <rahul.03111999@gmail.com>
2023-01-07 15:36:51 +0530

    added chakra ui

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,17 @@
-import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-   <SessionProvider session={session}>
-    <Component {...pageProps} />
-   </SessionProvider>
-  )
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
